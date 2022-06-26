da708b9b7697add339ca32c235768e3dd857f7f0
Mazen Elayeb <melayeb@anypli.com>
2022-06-26 11:07:57 +0100

    [PetStore]: Create a Pets Page By using Desgin Pattern container

---
+++ src/pages/Pets/Pets.container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Pets() {
+  return <div>Pets</div>;
+}
+
+// Language: typescript
+// Path: src/pages/Pets/Pets.container.tsx
+// Compare this snippet from src/index.tsx:
