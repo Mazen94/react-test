cf36a935179b5fde8283566c92dd4c537436df48
Mazen Elayeb <melayeb@anypli.com>
2022-06-26 11:09:37 +0100

    [PetStore]: Remove unused type

--- src/store/flags/reducers.ts
+++ src/store/flags/reducers.ts
@@ -5,9 +5,7 @@ export const INITIAL_STATE: FlagsState = {};
 
 export function FlagsReducer(state = INITIAL_STATE, action: StoreFlagAction) {
   const { type, payload } = action;
-  const matches = /(.*)_(LOADING|REFRESHING|ERROR|TRACKING|SHOWING|INVALID_TYPE|DISPLAYED|LOADED|HAS_ACCESS|DOWNLOADING|UNAVAILABLE|SHARING)/.exec(
-    type,
-  );
+  const matches = /(.*)_(LOADING|ERROR)/.exec(type);
 
   if (!matches) return state;
 
