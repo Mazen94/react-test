89aaf757026cc9b8be7f1e4684869ee05590885d
Mazen Elayeb <melayeb@anypli.com>
2022-06-26 10:39:33 +0100

    [PetStore]: Setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './store/rootStore';
+import thunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
